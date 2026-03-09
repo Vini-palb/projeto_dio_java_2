eb94d4f88fe03f9e83b050072debbe0beb0ef332
Vini-palb <viniciuspalb@gmail.com>
2026-03-09 16:22:51 -0300

    feat: implementação do Facade para simplificar acesso ao sistema
    - criação da classe ProjectService
    - centralização da lógica de busca de projetos
    - integração com as estratégias de busca

--- Projeto-poo-dio-2/facade/ProjectService.java
+++ Projeto-poo-dio-2/facade/ProjectService.java
@@ -1,1 +1,19 @@
-// Add your code here
+package facade;
+
+import strategy.ProjectSearchStrategy;
+import model.Project;
+
+import java.util.List;
+
+public class ProjectService {
+
+    private ProjectSearchStrategy strategy;
+
+    public void setStrategy(ProjectSearchStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public List<Project> search(String value) {
+        return strategy.search(value);
+    }
+}
