6eb397c05c73309299ff9964b36e2f715bee2687
Vini-palb <viniciuspalb@gmail.com>
2026-03-09 16:24:37 -0300

    feat: implementação da estratégia de busca por status
    - criação da classe SearchByStatus
    - implementação da interface ProjectSearchStrategy
    - filtro de projetos pelo status utilizando Stream API

--- Projeto-poo-dio-2/strategy/SearchByStatus.java
+++ Projeto-poo-dio-2/strategy/SearchByStatus.java
@@ -1,1 +1,20 @@
-// Add your code here
+package strategy;
+
+import database.ProjectDatabase;
+import model.Project;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SearchByStatus implements ProjectSearchStrategy {
+
+    public List<Project> search(String status) {
+
+        return ProjectDatabase
+                .getInstance()
+                .getProjects()
+                .stream()
+                .filter(p -> p.getStatus().equalsIgnoreCase(status))
+                .collect(Collectors.toList());
+    }
+}
