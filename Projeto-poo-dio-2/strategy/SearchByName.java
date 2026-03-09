d992b02f8b69b22dc41b0e20e3d0957a3512c098
Vini-palb <viniciuspalb@gmail.com>
2026-03-09 16:24:18 -0300

    feat: implementação da estratégia de busca por nome
    - criação da classe SearchByName
    - implementação da interface ProjectSearchStrategy
    - filtro de projetos pelo nome utilizando Stream API

--- Projeto-poo-dio-2/strategy/SearchByName.java
+++ Projeto-poo-dio-2/strategy/SearchByName.java
@@ -1,1 +1,20 @@
-// Add your code here
+package strategy;
+
+import database.ProjectDatabase;
+import model.Project;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SearchByName implements ProjectSearchStrategy {
+
+    public List<Project> search(String name) {
+
+        return ProjectDatabase
+                .getInstance()
+                .getProjects()
+                .stream()
+                .filter(p -> p.getName().equalsIgnoreCase(name))
+                .collect(Collectors.toList());
+    }
+}
