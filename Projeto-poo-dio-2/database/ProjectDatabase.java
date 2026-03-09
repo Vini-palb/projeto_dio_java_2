07038f63b05933d109c962b3441442ced2fe30f3
Vini-palb <viniciuspalb@gmail.com>
2026-03-09 16:22:02 -0300

    feat: implementação do banco de dados em memória com Singleton
    - criação da classe ProjectDatabase
    - implementação do padrão Singleton
    - adição de projetos de exemplo no banco em memória

--- Projeto-poo-dio-2/database/ProjectDatabase.java
+++ Projeto-poo-dio-2/database/ProjectDatabase.java
@@ -1,1 +1,32 @@
-// Add your code here
+package database;
+
+import model.Project;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectDatabase {
+
+    private static ProjectDatabase instance;
+
+    private List<Project> projects = new ArrayList<>();
+
+    private ProjectDatabase() {
+
+        projects.add(new Project("Sistema de Pagamentos", "EM_DESENVOLVIMENTO"));
+        projects.add(new Project("Portal Web", "EM_TESTES"));
+        projects.add(new Project("App Mobile", "CONCLUIDO"));
+    }
+
+    public static ProjectDatabase getInstance() {
+
+        if (instance == null) {
+            instance = new ProjectDatabase();
+        }
+
+        return instance;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+}
