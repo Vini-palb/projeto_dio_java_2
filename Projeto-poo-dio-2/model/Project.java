3d2f74e6f345c6060976be3e7f2c4c3199de4142
Vini-palb <viniciuspalb@gmail.com>
2026-03-09 16:20:13 -0300

    feat: criação da classe Project
    - implementação da classe que representa um projeto
    - atributos: name e status
    - criação de construtor e métodos getters

--- Projeto-poo-dio-2/model/Project.java
+++ Projeto-poo-dio-2/model/Project.java
@@ -1,1 +1,20 @@
-// Add your code here
+package model;
+
+public class Project {
+
+    private String name;
+    private String status;
+
+    public Project(String name, String status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
